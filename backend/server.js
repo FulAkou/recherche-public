c235d8407e51a5374f1824fd5b259bb52088e2d6
FulAkou <fulgencebeugre49@gmail.com>
2025-06-19 14:56:33 +0000

    Fix: update cors on backend

--- backend/server.js
+++ backend/server.js
@@ -13,12 +13,28 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
+const allowedOrigins = ["http://localhost:5173", "https://iviz-3.vercel.app"];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS policy: Origin not allowed"));
+      }
+    },
     credentials: true,
   })
 );
+
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     "https://iviz-3.vercel.app": true,
+//     credentials: true,
+//   })
+// );
 app.use(express.json());
 
 // Connexion à la base de données MongoDB
